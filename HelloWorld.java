26093167e56c8a6c900a2bc55eef9f6521b7cb0b
Santhosh.kumar <santhosh.v11@outlook.com>
2023-02-02 09:01:24 +0530

    Second changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
     public static void main (String[] args) {
         System.out.println("-------******* Hello World & Welcome to Jenkins Training*****----");
         System.out.println("--- Made some changes------");
-        
+        System.out.println("--- Made some changes for git example -----");
     }
 }
